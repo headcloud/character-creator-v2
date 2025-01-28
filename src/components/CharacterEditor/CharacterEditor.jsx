96883996b851681d4491b728184de831bdf83aa6
headcloud <mvg5715@gmail.com>
2025-01-28 15:01:43 +0300

    feat: implemented layout

--- src/components/CharacterEditor/CharacterEditor.jsx
+++ src/components/CharacterEditor/CharacterEditor.jsx
@@ -76,19 +76,19 @@ function App() {
             handleSelectOption={setClothesColor}
           />
         </div>
-      </MaxWidthWrapper>
-
-      <div className={styles.characterWrapper}>
-        <Character
-          body={body}
-          head={head}
-          face={face}
-          accessory={accessory}
-          skinColor={skinColor}
-          clothesColor={clothesColor}
-        />
-      </div>
-    </main>
+     </MaxWidthWrapper>
+        <div className={styles.characterWrapper}>
+          <Character
+            body={body}
+            head={head}
+            face={face}
+            accessory={accessory}
+            skinColor={skinColor}
+            clothesColor={clothesColor}
+          />
+        </div>
+ 
+   </main>
   );
 }
 
